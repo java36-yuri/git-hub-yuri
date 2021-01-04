fc22b4cdb164fec3743165dce588cca118099a65
Yuragranovsky <yuragranovsky@gmail.com>
2021-01-04 15:54:53 +0200

    initial code

---
+++ example-git-yuri/src/main/java/telran/spring/jpa/ExampleGitYuriApplication.java
@@ -0,0 +1,13 @@
+package telran.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitYuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitYuriApplication.class, args);
+	}
+
+}
---
+++ example-git-yuri/src/test/java/telran/spring/jpa/ExampleGitYuriApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.spring.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitYuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
